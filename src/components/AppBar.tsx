c5e1e8a713b3a4c977b9053ff1afb3269cbec89b
Harsh Vardhan Sharma <38874238+legend7bot@users.noreply.github.com>
2023-03-07 00:03:08 +0530

    ...

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 
 import FontSelector from './elements/FontSelector';
 import Toggler from './elements/Toggler';
@@ -12,17 +12,39 @@ interface AppBarProps {
 
 const AppBar: React.FC<AppBarProps> = (props) => {
   const { darkMode, setDarkMode, font, setFont } = props;
+  const [opacity, setOpacity] = React.useState<boolean>(false);
+
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setOpacity(true);
+    } else {
+      setOpacity(false);
+    }
+  };
+  console.log(opacity);
+  window.addEventListener('scroll', changeNavbarColor);
+
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('scroll', changeNavbarColor);
+    };
+  }, []);
 
   return (
-    <div className="z-[999] flex h-20 w-auto items-center justify-around rounded-full bg-[#eeeedd] dark:bg-[#1A1A2E]">
-      <Toggler
-        darkMode={darkMode}
-        setDarkMode={setDarkMode}
-      />
-      <FontSelector
-        font={font}
-        setFont={setFont}
-      />
+    <div className="flex h-20 w-auto">
+      <div
+        className={`fixed top-0 z-[999] flex h-20 w-[100vw] items-center justify-around rounded-full ${
+          opacity ? 'bg-[#eeeedd]/70' : 'bg-[#eeeedd]/100'
+        } ${opacity ? 'dark:bg-[#1A1A2E]/70' : 'dark:bg-[#1A1A2E]/100'}`}>
+        <Toggler
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+        />
+        <FontSelector
+          font={font}
+          setFont={setFont}
+        />
+      </div>
     </div>
   );
 };
