facd3f90e60e095dbe54355ce9f7bddd961d09b5
Harsh Vardhan Sharma <38874238+legend7bot@users.noreply.github.com>
2023-03-06 20:57:31 +0530

    responsiveness-optimization

--- src/components/elements/Dictionary/SearchBar.tsx
+++ src/components/elements/Dictionary/SearchBar.tsx
@@ -13,25 +13,25 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
   };
   return (
     <div>
-      <div className="relative flex h-16 rounded-full bg-[#eeeeee] dark:bg-[#0F3460] md:w-[50rem]">
+      <div className="relative flex h-16 w-[90vw] items-center justify-evenly rounded-full bg-[#eeeeee] p-1 dark:bg-[#0F3460] sm:w-[80vw] md:w-[50rem]">
         <form
-          className="shrink-0 basis-[100%]"
+          className="w-full rounded-full"
           onSubmit={(e) => {
             onSubmit(e, keyword);
           }}>
           <input
-            className="h-16 w-[95%] rounded-full bg-[#eeeeee] p-1 pl-4 pr-10 text-xl text-[#1A1A2E] focus:outline-none dark:bg-[#0F3460] dark:text-[#eeeeff]"
+            className="h-16 w-[100%] rounded-full bg-[#eeeeee] p-1 pl-4 text-xl tracking-wide text-[#1A1A2E] focus:outline-none dark:bg-[#0F3460] dark:text-[#eeeeff]"
             key="search-bar"
             value={keyword}
             placeholder={'Search a Word...'}
             onChange={(e) => onChange(e.target.value)}
           />
         </form>
-        <span
+        <div
           onClick={(e) => onSubmit(e, keyword)}
-          className="absolute right-1 top-[50%] flex h-14 w-14 translate-y-[-50%] items-center justify-center rounded-full text-indigo-800 hover:scale-125 hover:cursor-pointer dark:text-[#eeeeee]">
+          className="flex h-14 w-14 shrink-0 items-center justify-center rounded-full text-indigo-800 hover:scale-125 hover:cursor-pointer dark:text-[#eeeeee]">
           <FaSearch />
-        </span>
+        </div>
       </div>
     </div>
   );
